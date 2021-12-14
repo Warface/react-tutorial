396834f0e7253e25f37e4cc1ed6c566bb570de34
Carl Spooner <cspooner@bravad.ca>
2021-12-13 21:26:42 -0500

    Added Routes and Counter

--- src/Sections/Counter/Counter.js
+++ src/Sections/Counter/Counter.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 function Counter() {
-  return <h1>Counter page</h1>;
+
+  const savedCount = localStorage.getItem("count") ? localStorage.getItem("count") : 0;
+  const [count, setCount] =  React.useState(parseFloat(savedCount));
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
+
+  const updateCounter = () => {
+    setCount(count + 1)
+    localStorage.setItem("count", count + 1);
+  }
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={updateCounter}>
+        Click me
+      </button>
+    </div>
+  );
+
 }
 
 export default Counter;
